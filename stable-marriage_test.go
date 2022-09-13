1f336a7860b168af038cf6c2fce4a3219425b08a
Austin P. Maher <austinpmaher@yahoo.com>
2022-09-12 22:34:18 -0400

    Add tests for Solve and more test data combinations

--- stable-marriage_test.go
+++ stable-marriage_test.go
@@ -5,14 +5,79 @@ import (
 	"testing"
 )
 
-type StableMarriageTestData struct {
+var SIMILAR_MEN = map[string][]string{
+	"A": {"C", "D"},
+	"B": {"C", "D"},
+}
+
+var SIMILAR_WOMEN = map[string][]string{
+	"C": {"A", "B"},
+	"D": {"A", "B"},
+}
+var OPPOSITE_MEN = map[string][]string{
+	"A": {"C", "D"},
+	"B": {"D", "C"},
+}
+
+var OPPOSITE_WOMEN = map[string][]string{
+	"C": {"A", "B"},
+	"D": {"B", "A"},
+}
+
+var OPPOSITE_WOMEN_2 = map[string][]string{
+	"C": {"B", "A"},
+	"D": {"A", "B"},
+}
+
+type StableMarriageSolutionTestCase struct {
+	testcase   string
+	men, women PreferenceMatrix
+	isValid    bool
+}
+
+func runSolutionTest(tc StableMarriageSolutionTestCase, t *testing.T) {
+	var foundPlan = Solve(tc.men, tc.women)
+	var result, details = IsStableSolution(tc.men, tc.women, *foundPlan)
+
+	if result != tc.isValid {
+		t.Error(fmt.Sprintf("%s expected %t but got %t. %s", tc.testcase, tc.isValid, result, details))
+	} else {
+		t.Log(fmt.Sprintf("%s passed", tc.testcase))
+	}
+}
+
+func TestSolve(t *testing.T) {
+	runSolutionTest(StableMarriageSolutionTestCase{
+		testcase: "ss",
+		men:      SIMILAR_MEN,
+		women:    SIMILAR_WOMEN,
+		isValid:  true,
+	},
+		t)
+	runSolutionTest(StableMarriageSolutionTestCase{
+		testcase: "so2",
+		men:      SIMILAR_MEN,
+		women:    OPPOSITE_WOMEN_2,
+		isValid:  true,
+	},
+		t)
+	runSolutionTest(StableMarriageSolutionTestCase{
+		testcase: "oo2",
+		men:      OPPOSITE_MEN,
+		women:    OPPOSITE_WOMEN_2,
+		isValid:  true,
+	},
+		t)
+}
+
+type IsStableSolutionTestCase struct {
 	testcase     string
 	men, women   PreferenceMatrix
 	expectedPlan MarriagePlan
 	isValid      bool
 }
 
-func runTest(td StableMarriageTestData, t *testing.T) {
+func runIsStableTest(td IsStableSolutionTestCase, t *testing.T) {
 	var result, details = IsStableSolution(td.men, td.women, td.expectedPlan)
 
 	if result != td.isValid {
@@ -24,20 +89,10 @@ func runTest(td StableMarriageTestData, t *testing.T) {
 
 func TestIsStable(t *testing.T) {
 
-	var MEN = map[string][]string{
-		"A": {"C", "D"},
-		"B": {"C", "D"},
-	}
-
-	var WOMEN = map[string][]string{
-		"C": {"A", "B"},
-		"D": {"B", "A"},
-	}
-
-	runTest(StableMarriageTestData{
+	runIsStableTest(IsStableSolutionTestCase{
 		testcase: "valid test",
-		men:      MEN,
-		women:    WOMEN,
+		men:      SIMILAR_MEN,
+		women:    OPPOSITE_WOMEN,
 		expectedPlan: MarriagePlan{
 			{lhs: "A", rhs: "C"},
 			{lhs: "B", rhs: "D"},
@@ -46,10 +101,10 @@ func TestIsStable(t *testing.T) {
 	},
 		t)
 
-	runTest(StableMarriageTestData{
+	runIsStableTest(IsStableSolutionTestCase{
 		testcase: "invalid test",
-		men:      MEN,
-		women:    WOMEN,
+		men:      SIMILAR_MEN,
+		women:    OPPOSITE_WOMEN,
 		expectedPlan: MarriagePlan{
 			{lhs: "A", rhs: "C"},
 			{lhs: "B", rhs: "C"},
@@ -58,10 +113,22 @@ func TestIsStable(t *testing.T) {
 	},
 		t)
 
-	runTest(StableMarriageTestData{
+	runIsStableTest(IsStableSolutionTestCase{
+		testcase: "invalid test",
+		men:      SIMILAR_MEN,
+		women:    OPPOSITE_WOMEN,
+		expectedPlan: MarriagePlan{
+			{lhs: "A", rhs: "C"},
+			{lhs: "B", rhs: "C"},
+		},
+		isValid: false,
+	},
+		t)
+
+	runIsStableTest(IsStableSolutionTestCase{
 		testcase: "unstable test",
-		men:      MEN,
-		women:    WOMEN,
+		men:      SIMILAR_MEN,
+		women:    OPPOSITE_WOMEN,
 		expectedPlan: MarriagePlan{
 			{lhs: "A", rhs: "D"},
 			{lhs: "B", rhs: "C"},
